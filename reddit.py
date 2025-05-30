029137e7a2e1a48bbabaef8960a15eacb6585ae8
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-05-29 23:39:31 -0500

    Minado de tendencias en Reddit

---
+++ reddit.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests, csv
+
+URL = 'https://apewisdom.io/all-crypto'
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+criptos = soup.find_all('div', class_='company-name')
+criptos = str(criptos)
+criptos = criptos.replace('<div class="company-name">', '|')
+criptos = criptos.replace('</div>', '')
+criptos = criptos.replace('[', '')
+criptos = criptos.replace('</div>] ', '')
+criptoclean = criptos.split("|")
+criptoclean.pop(0)
+lista = []
+lista.append('criptoclean[0]')
+lista.append('criptoclean[1]')
+lista.append('criptoclean[2]')
+print(lista)
+print(criptoclean)
+
+with open('reddit.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',')
+    writer.writerow(criptoclean)
